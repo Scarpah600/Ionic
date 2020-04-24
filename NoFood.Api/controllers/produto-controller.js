658c9afbb39a178e38c98452a8db7d4510b2b508
Gustavo <mmg.services@hotmail.com>
2020-04-24 15:04:24 -0300

    atualizado

--- NoFood.Api/controllers/produto-controller.js
+++ NoFood.Api/controllers/produto-controller.js
@@ -2,34 +2,48 @@
 
 require('../models/produto-model');
 const repository = require('../repositories/produto-repository');
+const ctlBase = require('../bin/base/controller-base');
+const validation = require('../bin/helpers/validation');
+const _repo = new repository();
 
 function produtoController() {
 
 }
 
 produtoController.prototype.post = async (req, res) => {
-    let resultado = await new repository().create(req.body);
-    res.status(201).send(resultado);
+    let _validationContract = new validation();
+    _validationContract.isRequired(req.body.nome, 'O nome do produto é obrigatorio');
+    _validationContract.isRequired(req.body.descricao, 'O descricao do produto é obrigatorio');
+    _validationContract.isRequired(req.body.foto, 'A foto do produto é obrigatorio');
+    _validationContract.isRequired(req.body.preco, 'O preço do produto é obrigatorio');
+    if(req.body.preco)
+    _validationContract.isTrue(req.body.preco == 0,'O preço do produto deve ser maior que Zero.');
+
+    ctlBase.post(_repo,_validationContract,req,res);
 };
 
 produtoController.prototype.put = async (req, res) => {
-    let resultado = await new repository().update(req.params.id, req.body);
-    res.status(202).send(resultado);
+    let _validationContract = new validation();
+    _validationContract.isRequired(req.body.nome, 'O nome do produto é obrigatorio');
+    _validationContract.isRequired(req.body.descricao, 'O descricao do produto é obrigatorio');
+    _validationContract.isRequired(req.body.foto, 'A foto do produto é obrigatorio');
+    _validationContract.isRequired(req.body.preco, 'O preço do produto é obrigatorio');
+    if(req.body.preco)
+    _validationContract.isTrue(req.body.preco == 0,'O preço do produto deve ser maior que Zero.');
+
+    ctlBase.put(_repo,_validationContract,req,res);
 };
 
 produtoController.prototype.get = async (req, res) => {
-    let lista = await new repository().getAll();
-    res.status(200).send(lista);
+    ctlBase.get(_repo,req,res);
 };
 
 produtoController.prototype.getById = async (req, res) => {
-    let produto = await new repository().getById(req.params.id);
-    res.status(200).send(produto);
+    ctlBase.getById(_repo,req,res);
 };
 
 produtoController.prototype.delete = async (req, res) => {
-    let deletado = await new repository().delete(req.params.id);
-    res.status(204).send(deletado);
+    ctlBase.delete(_repo,req,res);
 };
 
 module.exports = produtoController;
